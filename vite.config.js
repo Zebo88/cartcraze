463b82fb6a862408b80e42c80960a2c6387b865f
Zach Fullmer <zachfullmer@Zachs-MacBook-Pro.local>
2024-03-20 16:58:15 -0600

    Changed vite config to use Render backend instead of localhost.

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://cartcraze.onrender.com/api/products',//added this to use Netlify to connect to the backend hosted on Render.
   plugins: [react()],
 })
