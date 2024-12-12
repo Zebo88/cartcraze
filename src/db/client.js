3d609dc5b101d8fe1e9d768c0dd36780b2b1a73f
Zach Fullmer <zachfullmer@Zachs-MacBook-Pro-2.local>
2024-12-11 18:07:53 -0700

    Changed db again

--- src/db/client.js
+++ src/db/client.js
@@ -2,7 +2,7 @@ import pkg from 'pg';
 const { Client } = pkg;
 
 // const url = "postgresql://cartcraze_db_eqcv_user:Jng89PQTfX779unLvhuPC8739mEVnKQw@dpg-cs4s01q3esus73alfo7g-a.oregon-postgres.render.com/cartcraze_db_eqcv";
-const url = "postgresql://cartcraze_db_eqcv_user:Jng89PQTfX779unLvhuPC8739mEVnKQw@dpg-cs4s01q3esus73alfo7g-a.oregon-postgres.render.com/cartcraze_db_eqcv";
+const url = "postgresql://cartcraze_db_2_user:bHOTlyrjMmB3ENlc3dejdOvywh99igFV@dpg-ctd3c2lumphs73bfnou0-a/cartcraze_db_2";
 const connectionString = process.env.DATABASE_URL || url;
 
 // const client = new Client({
